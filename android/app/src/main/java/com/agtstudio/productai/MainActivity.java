80b938209335db7203204ec72e98d57b3b0b6139
AGT Studio <tan.ahmet.at@gmail.com>
2026-09-18 02:06:49 +0300

    Add Android launcher activity

---
+++ android/app/src/main/java/com/agtstudio/productai/MainActivity.java
@@ -0,0 +1,41 @@
+package com.agtstudio.productai;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(false);
+        webView.getSettings().setAllowContentAccess(false);
+        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        setContentView(webView);
+        webView.loadUrl(BuildConfig.APP_URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}

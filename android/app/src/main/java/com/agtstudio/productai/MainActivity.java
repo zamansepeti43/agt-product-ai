08ca69f6e4672eacab78c420b152494677923079
AGT Studio <tan.ahmet.at@gmail.com>
2026-09-18 04:29:23 +0300

    Use reliable Android image picker for WebView uploads

--- android/app/src/main/java/com/agtstudio/productai/MainActivity.java
+++ android/app/src/main/java/com/agtstudio/productai/MainActivity.java
@@ -31,8 +31,9 @@ public class MainActivity extends Activity {
             public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> callback, FileChooserParams params) {
                 if (filePathCallback != null) filePathCallback.onReceiveValue(null);
                 filePathCallback = callback;
-                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
                 intent.setType("image/*");
                 intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
                 startActivityForResult(intent, 1001);
